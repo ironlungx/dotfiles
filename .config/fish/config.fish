42e3d9d09490fb0e5f76f17d1c839f039ff38ba3
IronLung <hwlooverhello@gmail.com>
2024-07-18 16:25:45 +0100

    Changed to relative line numbers & did some fish stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ set fish_greeting
 alias cd="z"
 alias ls="exa -A -l -U --no-permissions --no-user --color=auto --group-directories-first"
 source ~/.config/fish/aliases.fish
+fzf --fish | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,6 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set relativenumber")
 vim.g.mapleader=' '
-vim.wo.number = true
 
