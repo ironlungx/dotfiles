5d148a55807b41ebc16f670c4c6ea5858f67a317
IronLung <hwlooverhello@gmail.com>
2024-08-25 15:31:55 +0100

    hmm misc changes again

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -14,10 +14,10 @@ return {
     local wk = require("which-key")
 
     wk.add {
-      { "<C-h>",      "<C-W>h",                                                   desc = "Right Window" },
-      { "<C-j>",      "<C-W>j",                                                   desc = "Lower Window" },
-      { "<C-k>",      "<C-W>k",                                                   desc = "Upper Window" },
-      { "<C-l>",      "<C-W>l",                                                   desc = "Left Window" },
+      -- { "<C-h>",      "<C-W>h",                                                   desc = "Right Window" },
+      -- { "<C-j>",      "<C-W>j",                                                   desc = "Lower Window" },
+      -- { "<C-k>",      "<C-W>k",                                                   desc = "Upper Window" },
+      -- { "<C-l>",      "<C-W>l",                                                   desc = "Left Window" },
       { "<leader>l",  group = "lsp" },
       { "<leader>f",  group = "telescope" },
     }
