cd4277b1afcad71cfbeee1d3d2b0b67a7463082c
IronLung <hwlooverhello@gmail.com>
2024-09-21 10:37:08 +0100

    fixed a keybind in neovim

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -7,9 +7,12 @@ vim.cmd("set relativenumber")
 vim.cmd("set nowrap")
 vim.cmd("set guifont=JetbrainsMono\\ Nerd\\ Font:h11")
 
+vim.cmd("hi Folded guibg=NONE")
+vim.cmd("set cmdheight=2")
+
 vim.g.mapleader = " "
 vim.opt.showmode = false
 
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<leader>fs", ":w <CR>", { desc = "Save buffer" })
-vim.keymap.set("n", "<Esc>", ":nohl <CR>", { desc = "Remove Highlights" })
+vim.keymap.set("n", "<Esc>", ":nohl<CR>", { silent = true, desc = "Remove Highlights" })
